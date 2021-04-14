3c5b1736ca393485dc07ee27bde94f07eb7c27e3
Chris Goslin <cgoslin7@gmail.com>
2021-04-14 18:34:07 -0500

    organizing folder html

--- assets/server.js
+++ assets/server.js
@@ -78,7 +78,7 @@ app.delete('/api/notes/:id', (req, res) => {
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/add', (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
